f97d6e052b43690cce989731eb59c6dd32622e6b
saba-tohidi <saba.tohidikhanlar1999@gmail.com>
2021-05-19 13:11:45 -0700

    Create onlineshop.py

---
+++ onlineshop.py
@@ -0,0 +1,40 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+
+#class loginpage(object):
+
+
+
+class customer():
+
+    def init(self,Fullname,Password,confirmPassword,Phonenumber,email,Gender):
+        self.Fullname=Fullname
+        self.Password=Password
+        self.confirmPassword=confirmPassword
+        self.Phonnumber=Phonenumber
+        self.email=email
+        self.Gender=Gender
+
+    def fullname(self):
+        return self.Fullname
+    def password(self):
+        return self.Password
+    def confirm(self):
+        return self.confirmPassword
+    def phonenumber(self):
+        return self.Phonnumber
+    def email(self):
+        return self.email
+    def gender(self):
+        return self.Gender
+
+
+
+
+
+#class mainpage(object):
+
+
+
+#class CustomerRegistration(object):
