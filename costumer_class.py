7cb3736698df2b21f8901f605a3217fd2dfcee9c
Negarmkk <82811379+Negarmkk@users.noreply.github.com>
2021-06-03 17:00:06 +0430

    Update costumer_class.py

--- costumer_class.py
+++ costumer_class.py
@@ -1,1 +1,14 @@
-print('hello')
+import random 
+from seller_class import seller 
+
+class coustumer:
+    coustumer = dict()
+    store_inventory = dict()
+
+    def __init__(self, name, last_name, address ,phone_number=None, email=None):
+        self.name = name 
+        self.last_name = last_name
+        self.address = address
+        self.phone_number = phone_number 
+        self.email = email 
+        
