4d9d02523ded3bca1d3133f1c6a0cab421571b98
Negarmkk <82811379+Negarmkk@users.noreply.github.com>
2021-06-04 00:23:15 +0430

    Update costumer_class.py

--- costumer_class.py
+++ costumer_class.py
@@ -1,4 +1,6 @@
 import random 
+from Seller_Class import Seller
+
 
 class coustumer:
     coustumer = dict()
@@ -24,6 +26,37 @@ class coustumer:
         self.favorites = list()
         self.last_shopping = list()
         Costumer.costumers[self.costumer_id] = [name, last_name, address,phone_number, email]
+         @staticmethod
+    def list_stocks():
+        for goods in Costumer.store_inventory.keys():
+            yield str(goods) + " : " + str(Costumer.store_inventory[goods])
+
+    def charge_credit(self, amount):
+        self.credit = self.credit + amount
+
+    def add_to_cart(self, item, seller_id, price):
+        self.cart[item] = seller_id, price
+
+    def add_to_favorites(self, item):
+        self.favorites.append(item)
+
+    def pay(self):
+        total_fee = 0
+        for order in self.cart.values():
+            total_fee += order[1]
+        if total_fee > self.credit:
+            return "sorry, you have to increase your balance by: " + str(total_fee - self.credit) + "$"
+        else:
+            self.credit = self.credit - total_fee
+            self.last_shopping.append(self.cart)
+            self.cart = dict()
+            return "thank you for your shopping."
+
+    @staticmethod
+    def rate(seller_id, rating):
+        int(input("Please rate the seller from 1 to 5:"))
+        Seller.sellers_rates[seller_id].append(rating)
+
           
 
         
