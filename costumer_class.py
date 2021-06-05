3a1e633071e2d116491ecff0d9c0b2dca6747d11
Negarmkk <82811379+Negarmkk@users.noreply.github.com>
2021-06-05 23:33:50 +0430

    Update costumer_class.py

--- costumer_class.py
+++ costumer_class.py
@@ -13,19 +13,22 @@ class coustumer:
         self.phone_number = phone_number  # assigning costumer's phone number
         self.email = email  # assigning costumer's email
         random_id = random.randint(100000,1000000)# generating random 6 digit id number
+         '''hence the generated id is random, it is possible that they may coincide, so to avoid such situations we use
+         this while loop to check for existing duplicates'''
         flag = True# a flag to keep while loop going until we have a unique costumer id
         while flag is True:# assigning a unique costumer id to the new seller
-          if random_id not in Costumer.costumers.keys():
-             self.costumer_id = "CU" + str(random_id)
-             flag = False
-            else:
-                random_id = random.randint(100000, 1000000)
-                flag = True
-        self.credit = 0
-        self.cart = dict()
-        self.favorites = list()
-        self.last_shopping = list()
-        Costumer.costumers[self.costumer_id] = [name, last_name, address,phone_number, email]
+          if random_id not in Costumer.costumers.keys(): # checking existing costumers to find duplicate costumer id
+             self.costumer_id = "CU" + str(random_id) # no duplicates were found, so we assign costumer id
+             flag = False# now that the id is successfully assigned, we change the flag to stop while loop
+            else: # duplicate id were found
+                random_id = random.randint(100000, 1000000)# generating a new random costumer id
+        self.credit = 0# initial costumer's balance
+        self.cart = dict()# costumer's cart(shopping basket)
+        self.favorites = list() # costumer's favorites lists
+        self.last_shopping = list()  # costumer's historical shopping details
+         # adding the new costumer to the dictionary so that we can add them to database later
+        Costumer.costumers[self.costumer_id] = [name, last_name,address , phone_number, email]
+
          @staticmethod
     def list_stocks():
         for goods in Costumer.store_inventory.keys():
