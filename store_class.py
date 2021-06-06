27fbedb8e09084cdad60fed0c6083202944ab235
Negarmkk <82811379+Negarmkk@users.noreply.github.com>
2021-06-06 15:57:47 +0430

    Update store_class.py

--- store_class.py
+++ store_class.py
@@ -1,1 +1,8 @@
+class Store:
+    net_profit = 0  # variable to store the net profit of the store until this moment
+
+    def __init__(self, address, website_url, telephone_number):
+        self.address = address
+        self.website_url = website_url
+        self.telephone_number = telephone_number
 
