2e97b7c5235cd9d8654add9b6d4bac242df8b584
saba-tohidi <saba.tohidikhanlar1999@gmail.com>
2021-06-23 15:11:57 -0700

    Update onlineshop.py
    start pyqt

--- onlineshop.py
+++ onlineshop.py
@@ -1,170 +1,43 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class person():
-    def __init__(self, FirstName, LastName, gender, address, phonnumber, email,password)
-        self.__FirstName = FirstName
-        self.__LastName = LastName
-        self.__Address = Address
-        self.__Phonnumber = Phonnumber
-        self.__Email = Email
-        self.__password=password
 
-    #setters and getters
-    @property
-    def FirstName(self):
-        return self.__FirstName
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        MainWindow.setStyleSheet("background-color: rgb(85, 255, 255);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(260, 60, 281, 161))
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("index.jpg"))
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(474, 360, 81, 31))
+        self.pushButton.setStyleSheet("background-color: rgb(85, 85, 255);")
+        self.pushButton.setObjectName("pushButton")
+        self.customer = QtWidgets.QPushButton(self.centralwidget)
+        self.customer.setGeometry(QtCore.QRect(270, 360, 81, 31))
+        self.customer.setStyleSheet("background-color: rgb(255, 85, 0);")
+        self.customer.setObjectName("customer")
+        MainWindow.setCentralWidget(self.centralwidget)
 
-    @FirstName.setter
-    def FirstName(self, FirstName):
-        self.__FirstName = fullname
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    @property
-    def LastName(self):
-        return self.__LastName
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "فروشنده"))
+        self.customer.setText(_translate("MainWindow", "مشتری"))
 
-    @LastName.setter
-    def LastName(self, LastName):
-        self.__LastName = LastName
-    
-    @property
-    def address(self):
-        return self.__address
 
-    @address.setter
-    def address(self, address):
-        self.__address = address
-
-    @property
-    def phonnumber(self):
-        return self.__phonnumber
-
-    @phonnumber.setter
-    def phonnumber(self, phonnumber):
-        self.__phonnumber = phonnumber
-
-    @property
-    def email(self):
-        return self.__email
-
-    @email.setter
-    def email(self, email):
-        self.__email = email
-
-    @property
-    def gender(self): 
-    return self.__gender
-
-    @gender.setter
-    def gender(self,gender): 
-    if gender not in ['male', 'female']: 
-        raise Error('the value of gender should be [male or female] ')
-    self.__gender = gender
-    @property
-    def password(self):
-        return self.__password
-    @password.setter
-    def password(self,password):
-        self.__password
-#import person
-#class customer(person):
-    #def __init__(self,Fullname,Password,confirmPassword,Phonenumber,email,Gender):
-        #self.Fullname=Fullname
-        #self.Password=Password
-        #self.confirmPassword=confirmPassword
-        #self.Phonnumber=Phonenumber
-        #self.email=email
-        #self.Gender=Gender
-
-    #def fullname(self):
-    #    return self.Fullname
-    #def password(self):
-    #    return self.Password
-    #def confirm(self):
-    #    return self.confirmPassword
-    #def phonenumber(self):
-    #    return self.Phonnumber
-    #def email(self):
-    #    return self.email
-    #def gender(self):
-    #    return self.Gender
-
-class Item():
-    def __init__(self, name, description, price, code):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.code = code
-
-    def name(self):
-        return self.name
-
-    def description(self):
-        return self.description
-
-    def price(self):
-        return self.price
-
-    def code(self):
-        return self.code
-
-import Item
-class ShoppingBasket():
-    def __init__(self):
-        self.items = {} #A dictionary of all the items in the shopping basket: {item:quantity} 
-        self.checkout = False
-  
-    # add an item to the shopping basket  
-    def addItem(self,item,quantity=1):
-        if quantity > 0: 
-            #Check if the item is already in the shopping basket
-            if item in self.items:
-                self.items[item] += quantity
-            else: 
-                self.items[item] = quantity
-        else:
-            print("Invalid operation - Quantity must be a positive number!")
-
-      
-    # remove an item from the shopping basket (or reduce it's quantity)  
-    def removeItem(self,item,quantity=0):
-        if quantity<=0: 
-            #Remove the item
-            self.items.pop(item, None)
-        else:
-            if item in self.items:
-                if quantity<self.items[item]:
-                    #Reduce the required quantity for this item
-                    self.items[item] -= quantity
-                else:
-                    #Remove the item
-                    self.items.pop(item, None)
-          
-    # update the quantity of an item from the shopping basket  
-    def updateItem(self,item,quantity):
-        if quantity > 0: 
-            self.items[item] = quantity
-        else:
-            self.removeItem(item)
-
-  
-    # view/list the content of the basket.
-    def view(self):
-
-  
-    # calculate the total cost of the basket.
-    def getTotalCost(self):
-        totalCost = 0
-        for item in self.items:
-            quantity = self.items[item]
-            cost = quantity * item.price
-            totalCost += cost
-        return totalCost
-
-    
-    # empty the content of the basket
-    def reset(self):
-        self.items = {}
-    
-    # return whether the basket is empty or not:
-    def isEmpty(self):
-        return len(self.items)==0
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
